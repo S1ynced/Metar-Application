be397de9985fb18c610d6cf6099d46da3a9e5b02
S1ynced <s1yncedx@gmail.com>
2022-07-29 15:52:54 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,101 @@
+var token = config.API_TOKEN;
+
+// alert("It's Updated v1!");
+//Detecting Keyboard Press
+document.addEventListener("keydown", function (event) {
+  var keypress = event.key;
+  // console.log(keypress);
+});
+
+//Departure ICAO Data
+document
+  .querySelector(".departure-icao")
+  .addEventListener("keydown", function (event) {
+    if (event.keyCode === 13) {
+      var departureICAO = document.querySelector(".departure-icao").value;
+
+      var departureRequest = new XMLHttpRequest();
+
+      departureRequest.open(
+        "GET",
+        "https://avwx.rest/api/metar/" + departureICAO
+      );
+      departureRequest.setRequestHeader(
+        "Authorization",
+        "token"
+      );
+
+      departureRequest.onreadystatechange = function () {
+        if (this.readyState === 4) {
+          console.log("Status:", this.status);
+          console.log("Headers:", this.getAllResponseHeaders());
+          // console.log('Body:', this.responseText);
+        }
+      };
+
+      departureRequest.onload = () => {
+        const data = JSON.parse(departureRequest.response);
+        console.log(data);
+
+        document.querySelector(".departure-air-pressure-data").innerHTML =
+          data.altimeter.value + " " + data.units.altimeter;
+        document.querySelector(".departure-wind-speed-data").innerHTML =
+          data.wind_direction.repr +
+          " / " +
+          data.wind_speed.value +
+          " " +
+          data.units.wind_speed +
+          "s";
+        document.querySelector(".departure-temperature-data").innerHTML =
+          data.temperature.value + "°" + data.units.temperature;
+        document.querySelector(".departure-dew-point-data").innerHTML =
+          data.dewpoint.value + "°" + data.units.temperature;
+      };
+      departureRequest.send();
+    } else console.log(event.keyCode);
+  });
+
+//ARRIVAL ICAO DATA
+document
+  .querySelector(".arrival-icao")
+  .addEventListener("keydown", function (event) {
+    if (event.keyCode === 13) {
+      var arrivalICAO = document.querySelector(".arrival-icao").value;
+
+      var arrivalRequest = new XMLHttpRequest();
+
+      arrivalRequest.open("GET", "https://avwx.rest/api/metar/" + arrivalICAO);
+      arrivalRequest.setRequestHeader(
+        "Authorization",
+        "token"
+      );
+
+      arrivalRequest.onreadystatechange = function () {
+        if (this.readyState === 4) {
+          console.log("Status:", this.status);
+          console.log("Headers:", this.getAllResponseHeaders());
+          // console.log('Body:', this.responseText);
+        }
+      };
+
+      arrivalRequest.onload = () => {
+        const data = JSON.parse(arrivalRequest.response);
+        console.log(data);
+
+        document.querySelector(".arrival-air-pressure-data").innerHTML =
+          data.altimeter.value + " " + data.units.altimeter;
+        document.querySelector(".arrival-wind-speed-data").innerHTML =
+          data.wind_direction.repr +
+          " / " +
+          data.wind_speed.value +
+          " " +
+          data.units.wind_speed +
+          "s";
+        document.querySelector(".arrival-temperature-data").innerHTML =
+          data.temperature.value + "°" + data.units.temperature;
+        document.querySelector(".arrival-dew-point-data").innerHTML =
+          data.dewpoint.value + "°" + data.units.temperature;
+      };
+      arrivalRequest.send();
+    } else console.log(event.keyCode);
+  });
